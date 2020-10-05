b5d51e8041a7ae16fd8632934c0952e32ca66fd1
braykh <holbraykh@gmail.com>
2020-10-05 13:54:20 +0300

    twilio: client.messages.create

--- controllers/message.js
+++ controllers/message.js
@@ -1,4 +1,7 @@
 const Message = require('../models/message');
+const accountSid = 'AC3f7d99ae57d5077cdaddb4b12a4992fd';
+const authToken = 'e3b2aaecf0ab5a141d542ca88b45c86d';
+const client = require('twilio')(accountSid, authToken);
 
 exports.getSendMessage = (req, res, next) => {
     res.render('send-message', { 
@@ -9,19 +12,27 @@ exports.getSendMessage = (req, res, next) => {
 
 exports.postSendMessage = (req, res, next) => {
     const {message, phone} = req.body;
-    const _message = new Message({
-        message,
-        phone
-    });
-    _message
-        .save()
-        .then(result => {
-            console.log('Message sent successfully.');
-            res.redirect('/messages');
-        })
-        .catch(err => {
-            console.log(err);
+
+    client.messages.create({
+        from: 'whatsapp:+14155238886',
+        body: message,
+        to: 'whatsapp:' + phone
+    })
+    .then(wa_message => {
+        console.log(wa_message);
+        const _message = new Message({
+            message: wa_message.body,
+            phone: wa_message.to
         });
+        return _message.save();
+    })
+    .then(result => {
+        console.log('Message sent successfully.');
+        res.redirect('/messages');
+    })
+    .catch(err => {
+        console.log(err);
+    });
     
 };
 
